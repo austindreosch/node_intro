303267982b86d12bc7c35f6830331a762d12b355
Austin Dreosch <austindreosch@gmail.com>
2023-02-10 13:33:10 -0800

    hg

---
+++ step1.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const process = require('process');
+
+
+
+function cat(path){
+    fs.readFile(path, 'utf8', (err, data) => {
+        if(err){
+            console.log(`ERROR: ${err} on PATH: ${path}`);
+            process.exit(1);
+        }
+        else {
+            console.log(data);
+        }
+    })
+}
+
+cat(process.argv[2]);
---
+++ step2.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+
+
+// function cat(path){
+//     fs.readFile(path, 'utf8', (err, data) => {
+//         if(err){
+//             console.error(`ERROR: ${err} on PATH: ${path}`);
+//             process.exit(1);
+//         }
+//         else {
+//             console.log(data);
+//         }
+//     })
+// }
+
+function cat(path) {
+    fs.readFile(path, 'utf8', function(err, data) {
+      if (err) {
+        console.error(`Error reading ${path}: ${err}`);
+        process.exit(1);
+      } else {
+        console.log(data);
+      }
+    });
+  }
+
+
+async function webCat(url){
+    try {
+        let call = await axios.get(url);
+        console.log(call.data);
+    }
+    catch {
+        console.error(`Error at URL: ${url} - ${err}`);
+        process.exit(1);
+    }
+}
+
+let path = process.argv[2];
+
+if (path.slice(0,4) === 'http') {
+    webCat(path);
+}
+else {
+    cat(path);
+}
